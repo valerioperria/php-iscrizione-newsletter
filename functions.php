161b4c6aca782250b1eb3b7cb1065a5703f24545
valerioperria <valerio.perria@gmail.com>
2023-12-13 17:24:49 +0100

    feat. Milestone_3

--- functions.php
+++ functions.php
@@ -2,13 +2,16 @@
 
 if (empty($_GET['mail'])) {
     $result = 'inserisci mail';
+    $alert = 'alert-info';
 } else {
     if (isset($_GET['mail'])) {
         $mail = $_GET['mail'];
         if (str_contains($mail, '@')) {
             $result = 'good job';
+            $alert = 'alert-success';
         } else {
             $result = 'bad job';
+            $alert = 'alert-danger';
         }
     }
 }
--- index.php
+++ index.php
@@ -9,13 +9,16 @@ include __DIR__ . '/functions.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-iscrizione-newsletter</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
     <form action="index.php" method="GET">
-        <label for="mail">Inserisci e-mail</label>
-        <input type="text" id="mail" name="mail">
-        <p><?php echo $result ?></p>
+        <div class="container mt-5">
+            <label for="mail">Inserisci e-mail</label>
+            <input type="text" id="mail" name="mail" class="mb-3">
+            <div class="alert <?php echo $alert ?>" role="alert"><?php echo $result ?></div>
+        </div>
     </form>
 </body>
 
